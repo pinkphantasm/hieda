99694270257e6aa7ffc56e5a1638b9efe562bf19
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-13 16:49:39 +0300

    refactor(upload, api): add htmx integration

--- src/upload_service/app/routers/api.py
+++ src/upload_service/app/routers/api.py
@@ -1,7 +1,7 @@
 import time
 
-from fastapi import APIRouter
-from fastapi import UploadFile
+from fastapi import APIRouter, UploadFile
+from fastapi.responses import HTMLResponse
 
 from ..config import s3, config
 
@@ -14,8 +14,15 @@ async def health():
     return {"details": "All systems operationals"}
 
 
-@router.post("/api/upload")
+@router.post("/api/upload", response_class=HTMLResponse)
 async def upload(file: UploadFile):
     filename = f"{int(time.time())}-{file.filename}"
     s3.upload_fileobj(file.file, config.bucket, filename)
-    return f"https://storage.yandexcloud.net/hieda/{filename}"
+    file_url = f"https://storage.yandexcloud.net/hieda/{filename}"
+
+    return HTMLResponse(
+        content=f"""<div class="clip-area">
+    <span class="clip-area-text">{file_url}</span>
+    <button type="button" class="clip-area-button">Копировать</button>
+</div>"""
+    )
