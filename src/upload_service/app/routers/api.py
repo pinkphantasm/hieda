5094f86a0e511f338bfda26cbf4029c7919180e6
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-13 15:51:47 +0300

    refactor(upload): separate api router

---
+++ src/upload_service/app/routers/api.py
@@ -0,0 +1,21 @@
+import time
+
+from fastapi import APIRouter
+from fastapi import UploadFile
+
+from ..config import s3, config
+
+
+router = APIRouter()
+
+@router.get('/api/health', status_code=200)
+async def health():
+    return {'details': 'All systems operationals'}
+
+
+@router.post('/api/upload')
+async def upload(file: UploadFile):
+    filename = f'{int(time.time())}-{file.filename}'
+    s3.upload_fileobj(file.file, config.bucket, filename)
+    return f"https://storage.yandexcloud.net/hieda/{filename}"
+
