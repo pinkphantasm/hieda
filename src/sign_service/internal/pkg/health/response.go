ff323eb08b272c8f14da5d1fb229c70e6ab65518
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-13 23:18:35 +0300

    refactor(sign): use go

---
+++ src/sign_service/internal/pkg/health/response.go
@@ -0,0 +1,14 @@
+// Package health provides service status response.
+package health
+
+// ServiceResponse represents status response from the service.
+type ServiceResponse struct {
+	Details string `json:"details"` // Details provided by the service.
+}
+
+// NewResponse returns a new instance of ServiceResponse.
+func NewResponse(details string) *ServiceResponse {
+	return &ServiceResponse{
+		Details: details,
+	}
+}
--- src/sign_service/src/main.rs
+++
@@ -1,5 +0,0 @@
-mod templates;
-
-fn main() {
-    println!("Hello, world!");
-}
